479f7aa8e50dbcfaebd3ca63c99b68f056afd6da
Daniel Cutaneo <14dcutaneo@gmail.com>
2021-10-16 16:11:38 -0400

    Added result limiter to 5 with itemCounter variable

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -3,6 +3,7 @@ async function windowActions() {
   const resultData = await request.json();
   const ACCESS_TOKEN = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
   const mymap = L.map('mapid').setView([38.9897, -76.9378], 12);
+  let itemCounter = 0;
 
   L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${ACCESS_TOKEN}`, {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -22,9 +23,11 @@ async function windowActions() {
   function displayMatches(event) {
     const matchArray = findMatches(event.target.value, resultData);
     const html = matchArray.map((place) => {
-      if (!event.target.value) {
+      if (!event.target.value || itemCounter > 4) {
         document.querySelector('.suggestions').innerHTML = '';
       } else {
+        itemCounter += 1;
+        console.log(itemCounter);
         return `
                 <li>
                     <span class = "name">${place.name}<br></span>
@@ -38,6 +41,7 @@ async function windowActions() {
       }
     }).join('');
     suggestions.innerHTML = html;
+    itemCounter = 0;
   }
 
   const searchInput = document.querySelector('.search');
